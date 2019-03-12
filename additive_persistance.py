0dcc39b4b747b97f4dbd73fbca850b1ca9e7d1c2
= <=>
2019-03-12 18:24:06 -0400

    Added input file and created a working prototype

--- additive_persistance.py
+++ additive_persistance.py
@@ -1,20 +1,29 @@
-f = open("input.txt", "r")
-for line in f:
-    additive_persistance(line)
-f.close()
-
 #Return the additive_persistance of the input
 #Precondition: number is of type String
 def additive_persistance(number):
     assert type(number) == type("str"), "Input must be a String!"
+    
     itterations = 0
     while len(number) > 1:
         number = sum_digits(number)
         itterations += 1
     return itterations
 
-#Return the sum of digits of the input
+#Return the sum of digits of the input as a String
 #Precondition: number is of type String
 def sum_digits(number):
     assert type(number) == type("str"), "Input must be a String!"
-    return sum([int(char) for char in number])
+    
+    _sum = 0
+    for i in range(len(number)):
+        _sum += int(number[i])
+    return str(_sum)
+
+def run():
+    f = open("input.txt", "r")
+    for line in f:
+        result = additive_persistance(line.rstrip())
+        print "Input: %s \nItterations: %s" % (line.rstrip(), result)
+    f.close()
+
+run()
