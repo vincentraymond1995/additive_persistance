74ce5c3997c4f572387a747ec9c41083a95b57a0
= <=>
2019-03-13 10:21:37 -0400

    Test

--- additive_persistance.py
+++ additive_persistance.py
@@ -1,33 +1,47 @@
 def run():
-    f = open("input.txt", "r")
-    for line in f:
+    file_ = open("input.txt", "r")
+    for line in file_:
         number = line.rstrip()
         result = additive_persistance(number)
         print "Input: %s" % (number)
         print "Itterations: %s" % (result)
         print "\n"
-    f.close()
+    file_.close()
 
-#Return the additive_persistance of the input
-#Precondition: number is of type String
 def additive_persistance(number):
+    '''
+        Return the additive_persistance of the input.
+
+        Args:
+            number (str): The input value
+
+        Returns:
+            str: The additive_persistance of the input
+    '''
     assert type(number) == type("str"), "Input must be a String!"
-    
+
     itterations = 0
     while len(number) > 1:
         number = sum_digits(number)
         itterations += 1
     return itterations
 
-#Return the sum of digits of the input as a String
-#Precondition: number is of type String
 def sum_digits(number):
+    '''
+        Returns the sum of the digits of the input.
+
+        Args:
+            number (str): The input value
+
+        Returns:
+            str: The sum of the digits of the input
+    '''
     assert type(number) == type("str"), "Input must be a String!"
-    
-    _sum = 0
-    for i in range(len(number)):
-        _sum += int(number[i])
-    return str(_sum)
+
+    sum_ = 0
+    for digit in number:
+        sum_ += int(digit)
+    return str(sum_)
 
 
 run()
